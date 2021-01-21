2556f6d28d8079487b052de7431a3f4c808d6ef9
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-01-20 22:10:24 -0500

    configuration for mongoose connection

--- server.js
+++ server.js
@@ -15,12 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
 });
+connection.on("error", (err) => {
+  console.log("Mongoose connection error:", err);
+});
+
 
 // routes
 app.use(require("./routes/api.js"));
